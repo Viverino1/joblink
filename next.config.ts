08ef107b68700185097831ff7634dae0a46df3ea
Vivek Maddineni <vivekmaddineni@Viveks-MacBook-Pro-2.local>
2025-04-11 11:33:08 -0500

    disabled eslint

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  eslint: {
+    // Warning: This allows the use of `any` type.
+    // Consider using more specific types instead of `any` for better type safety.
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
